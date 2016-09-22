d0418bdd0bd033e71931238ab9630df015a37068
paul <paul.horvath@ooyala.com>
2016-09-22 15:17:16 +0200

    added the project

---
+++ src/main/java/com/ooyala/jenkins/plugins/pipelineaggregatorview/PipelineAggregator.java
@@ -0,0 +1,203 @@
+package com.ooyala.jenkins.plugins.pipelineaggregatorview;
+
+import hudson.Extension;
+import hudson.model.*;
+import hudson.security.Permission;
+import hudson.util.RunList;
+import jenkins.model.Jenkins;
+import net.sf.json.JSONObject;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+import org.kohsuke.stapler.export.Exported;
+import org.kohsuke.stapler.export.ExportedBean;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+/**
+ * Created by paul on 20/09/16.
+ */
+@SuppressWarnings("unused")
+@ExportedBean
+public class PipelineAggregator extends View {
+
+   private transient int getBuildsLimit;
+
+   private String viewName;
+
+   private int fontSize;
+
+   private int buildHistorySize;
+
+   private boolean useCondensedTables;
+
+
+
+   private String filterRegex;
+
+   @DataBoundConstructor
+   public PipelineAggregator(String name, String viewName) {
+      super(name);
+      this.viewName = viewName;
+      this.fontSize = 16;
+      this.buildHistorySize = 16;
+      this.useCondensedTables = false;
+      this.filterRegex = null;
+   }
+
+   protected Object readResolve() {
+      if (getBuildsLimit == 0)
+         getBuildsLimit = 250;
+
+      if (fontSize == 0)
+         fontSize = 16;
+
+      if (buildHistorySize == 0)
+         buildHistorySize = 16;
+
+      return this;
+   }
+
+   @Override
+   public Collection<TopLevelItem> getItems() {
+      return new ArrayList<TopLevelItem>();
+   }
+
+   public int getFontSize() {
+      return fontSize;
+   }
+
+   public int getBuildHistorySize() {
+      return buildHistorySize;
+   }
+
+   public boolean isUseCondensedTables() {
+      return useCondensedTables;
+   }
+
+   public String getTableStyle() {
+      return useCondensedTables ? "table-condensed" : "";
+   }
+
+   public String getFilterRegex() {
+      return filterRegex;
+   }
+
+
+   @Override
+   protected void submit(StaplerRequest req) throws ServletException, IOException {
+      JSONObject json = req.getSubmittedForm();
+      this.fontSize = json.getInt("fontSize");
+      this.buildHistorySize = json.getInt("buildHistorySize");
+      this.useCondensedTables = json.getBoolean("useCondensedTables");
+      if (json.get("useRegexFilter") != null) {
+         String regexToTest = req.getParameter("filterRegex");
+         try {
+            Pattern.compile(regexToTest);
+            this.filterRegex = regexToTest;
+         } catch (PatternSyntaxException x) {
+            Logger.getLogger(ListView.class.getName()).log(Level.WARNING, "Regex filter expression is invalid", x);
+         }
+      } else {
+         this.filterRegex = null;
+      }
+      save();
+   }
+
+   @Override
+   public Item doCreateItem(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
+      return Jenkins.getInstance().doCreateItem(req, rsp);
+   }
+
+   @Override
+   public boolean contains(TopLevelItem item) {
+      return false;
+   }
+
+   @Override
+   public boolean hasPermission(final Permission p) {
+      return true;
+   }
+
+   /**
+    * This descriptor class is required to configure the View Page
+    */
+   @Extension
+   public static final class DescriptorImpl extends ViewDescriptor {
+      @Override
+      public String getDisplayName() {
+         return
+            "PipelineAggregator";
+      }
+   }
+
+   public Api getApi() {
+      return new Api(this);
+   }
+
+   @Exported(name = "builds")
+   public Collection<Build> getBuildHistory() {
+      List<WorkflowJob> jobs = Jenkins.getInstance().getAllItems(WorkflowJob.class);
+      RunList builds = new RunList(jobs).limit(getBuildsLimit);
+      ArrayList<Build> l = new ArrayList<Build>();
+      Pattern r = filterRegex != null ? Pattern.compile(filterRegex) : null;
+
+      for (Object b : builds) {
+         Run build = (Run) b;
+         Job job = build.getParent();
+         // Skip Maven modules. They are part of parent Maven project
+         if (job.getClass().getName().equals("hudson.maven.MavenModule"))
+            continue;
+
+         // If filtering is enabled, skip jobs not matching the filter
+         if (r != null && !r.matcher(job.getName()).find())
+            continue;
+
+         Result result = build.getResult();
+         l.add(new Build(job.getName(),
+            build.getFullDisplayName(),
+            build.getNumber(),
+            build.getStartTimeInMillis(),
+            build.getDuration(),
+            result == null ? "BUILDING" : result.toString()));
+      }
+
+      return l;
+   }
+
+   @ExportedBean(defaultVisibility = 999)
+   public class Build {
+      @Exported
+      public String jobName;
+      @Exported
+      public String buildName;
+      @Exported
+      public int number;
+      @Exported
+      public long startTime;
+      @Exported
+      public long duration;
+      @Exported
+      public String result;
+
+      public Build(String jobName, String buildName, int number, long startTime, long duration, String result) {
+         this.jobName = jobName;
+         this.buildName = buildName;
+         this.number = number;
+         this.startTime = startTime;
+         this.duration = duration;
+         this.result = result;
+      }
+   }
+
+}
---
+++ src/main/webapp/js/pipeline-aggregator.js
@@ -0,0 +1,104 @@
+function format_date(dt) {
+   return dt.getFullYear() + '-' + (dt.getMonth() < 9 ? '0' : '') + (dt.getMonth() + 1) + '-' + (dt.getDate() < 10 ? '0' : '') + dt.getDate() + ' ' + (dt.getHours() < 10 ? '0' : '') + dt.getHours() + ':' + (dt.getMinutes() < 10 ? '0' : '') + dt.getMinutes() + ':' + (dt.getSeconds() < 10 ? '0' : '') + dt.getSeconds();
+}
+$.ajaxSetup({
+   async: false
+});
+function format_interval(iv) {
+   if (iv < 1000) {
+      return iv + 'ms';
+   }
+
+   ivStr = '';
+   // Days
+   if (iv > 86400000) {
+      ivStr = Math.floor(iv / 86400000) + 'd';
+      iv = iv - Math.floor(iv / 86400000) * 86400000;
+   }
+   // Hours
+   if (iv > 3600000) {
+      ivStr += ' ' + Math.floor(iv / 3600000) + 'h';
+      iv = iv - Math.floor(iv / 3600000) * 3600000;
+   }
+   // Minutes
+   if (iv > 60000) {
+      ivStr += ' ' + Math.floor(iv / 60000) + 'm';
+      iv = iv - Math.floor(iv / 60000) * 60000;
+   }
+   // Seconds
+   if (iv > 1000)
+      ivStr += ' ' + Math.floor(iv / 1000) + 's';
+   return ivStr;
+}
+
+
+function reload_jenkins_build_history(tableSelector, viewUrl, buildHistorySize) {
+   $.getJSON(viewUrl + '/api/json', function (data) {
+      // Remove all existing rows
+      $(tableSelector + ' tbody').find('tr').remove();
+      i = 0;
+      $.each(data.builds, function (key, val) {
+         i++;
+         if (i > buildHistorySize) {
+            return;
+         }
+         dt = new Date(val.startTime + val.duration);
+         authors='<ul>';
+         buildName = val.buildName.replace(/(.*) #.*/, '$1');
+         bame = '<a role="button" href="'+jenkinsUrl+'/job/'+buildName+'/'+val.number+'/" class="btn">'+buildName+'</a>';
+         stages = '<div class="btn-group" role="group">'
+         $.getJSON(jenkinsUrl + '/job/' + val.buildName.replace(/(.*) #.*/, '$1') +'/'+val.number+"/wfapi//describe", function (data) {
+               if ( data.stages.length > 0) {
+                  authors='<ul class="list-unstyled">';
+                  if(typeof data._links.changesets !== 'undefined' )
+                  {
+                     $.getJSON(jenkinsUrl+data._links.changesets.href.replace('jenkins',''),function(data){
+                        for(change in data){
+                           for(commit in data[change].commits){
+                              text = '<strong>'+data[change].commits[commit].authorJenkinsId+'</strong> '+data[change].commits[commit].message;
+                              authors+='<li class="small">'+text+'</li>';
+                           }
+                        }
+
+                     });
+                  }else {
+                     authors+='<li class="small">No Changes</li>'
+                  }
+                  authors+='</ul>';
+                  for (stage in data.stages) {
+                     switch (data.stages[stage].status) {
+                        case 'SUCCESS':
+                           classes = 'btn-success';
+                           tableClass='sucess'
+                           break;
+                        case 'FAILED':
+                           classes='btn-danger'
+                           break;
+                        case 'ABORTED':
+                           classes = 'btn-warning';
+                           tableClass='warning'
+                        case 'UNSTABLE':
+                           classes = 'btn-warning';
+                           tableClass='danger'
+                           break;
+                        case 'IN_PROGRESS':
+                           classes = 'info invert-text-color';
+                           tableClass='info'
+                           break;
+                        default:
+                           classes = '';
+                     }
+
+                     stages += '<button type="button" class="btn ' + classes + '">' + data.stages[stage].name + '</button>';
+                  }
+               }
+            stages += '</div>'
+
+            newRow = '<tr><td class="text-left">' + bame + '</td><td class="text-left">'+stages+'</td><td>' +authors+ '</td><td>' + val.number + '</td><td>' + format_date(dt) + '</td><td>' + format_interval(val.duration) + '</td></trcla>';
+            $(tableSelector + ' tbody').append(newRow);
+         });
+
+      });
+   });
+}
+
