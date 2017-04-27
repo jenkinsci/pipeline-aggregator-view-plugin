555572321667bdd15fda58e63f8acb58e5c32311
Fajran Iman Rusadi <fajran@gmail.com>
2017-04-27 06:51:56 +0200

    Remove unused imports

--- src/test/java/com/ooyala/jenkins/plugins/pipelineaggregatorview/PipelineAggregatorTest.java
+++ src/test/java/com/ooyala/jenkins/plugins/pipelineaggregatorview/PipelineAggregatorTest.java
@@ -5,27 +5,16 @@ package com.ooyala.jenkins.plugins.pipelineaggregatorview;
 
 import static org.mockito.Mockito.when;
 
-import hudson.model.AbstractBuild;
-import hudson.model.ItemGroup;
-import hudson.util.RunList;
-import javafx.util.Duration;
-import jenkins.model.Jenkins;
-import jenkins.util.TimeDuration;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.junit.Assert;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.jvnet.hudson.test.JenkinsRule;
-import org.kohsuke.stapler.StaplerRequest;
-import org.kohsuke.stapler.StaplerResponse;
 import org.mockito.Mockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.regex.Pattern;
 
